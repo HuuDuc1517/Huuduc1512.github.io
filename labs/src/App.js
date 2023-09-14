057a067f5725a00eec03c9c83f07c0faf88ea2da
Mac <mac@MacBook-Pro-cua-Mac.local>
2023-09-14 21:42:46 +0700

    lab2

--- labs/src/App.js
+++ labs/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Clock from './components/clock/index';
 import Sum from './components/sum/index';
+import WorldClock from './components/world-clock';
 
 function App() {
   return (
     <div className="App">
-      <Clock/>
-      <Sum/>
+      {/* <Clock/> */}
+
+      {/* <Sum/> */}
+    
+      <WorldClock />
     </div>
   );
 }
---
+++ labs/src/components/world-clock/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function WorldClock() {
+    return (
+        <div className='world-clock'>
+            <div className="city-name">HaNoi</div>
+            <div className="city-clock">21:00</div>
+        </div>
+    )
+}
+export default WorldClock
