e83eb63ea1a18d573a862e197f6fd726ef58aef1
Mac <mac@MacBook-Pro-cua-Mac.local>
2023-09-14 20:49:24 +0700

    a

--- labs/src/components/sum/index.js
+++ labs/src/components/sum/index.js
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 
 function Sum() {
-    const [soA, setSoA] = useState(0)
+    const [soA, setSoA] = useState(0)//tính số A
+    const [soB, setSoB] = useState(0)//tính số B
+    const [totalAB, setTotal]=useState(0)//hàm tính tổng
+    console.log('so A=', soA)
+
     function handleAChange(e) {
-        console.log(e.target.value)
+        setSoA(e.target.value)
     }
+
+    function handleBChange(e) {
+        setSoB(e.target.value)
+    }
+    
+    function total(e) {
+        setTotal(Number(soA) + Number(soB)) //từ chuỗi string chuyển qua number
+    }
+
     return(
         <div>
             <input type='number' onChange={handleAChange}/>
-            <input type='number'/>
-            <button>Tính Tổng</button>
+            <input type='number' onChange={handleBChange}/>
+            <button onClick={total}>Tính Tổng</button>
+            <p>{soA} + {soB} = {totalAB}</p>
         </div>
     )
 }
