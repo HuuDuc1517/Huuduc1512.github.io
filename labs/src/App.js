db963ca681c8b290aa5d51f7543f0e3db1c29862
Mac <mac@MacBook-Pro-cua-Mac.local>
2023-09-16 21:51:36 +0700

    lab2

--- labs/src/App.js
+++ labs/src/App.js
@@ -3,15 +3,40 @@ import './App.css';
 import Clock from './components/clock/index';
 import Sum from './components/sum/index';
 import WorldClock from './components/world-clock';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+import * as React from 'react';
+import Box from '@mui/material/Box';
 
 function App() {
+  const [value, setValue] = React.useState(30);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
       {/* <Clock/> */}
 
       {/* <Sum/> */}
     
-      <WorldClock />
+      {/* <WorldClock tenThanhPho={'Ha Noi'} muiGio={7}/>
+      <WorldClock tenThanhPho={'HongKong'} muiGio={8}/>
+      <WorldClock tenThanhPho={'Croatia'} muiGio={1}/>
+      <WorldClock tenThanhPho={'Korea'} muiGio={9}/> */}
+
+      {/* <Button variant="contained">Đăng Nhập</Button> */}
+      <Box sx={{ width: 200 }}>
+        <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+          <VolumeDown />
+          <Slider aria-label="Volume" value={value} onChange={handleChange} />
+          <VolumeUp />
+        </Stack>
+        <Slider disabled defaultValue={30} aria-label="Disabled slider" />
+      </Box>
     </div>
   );
 }
--- labs/src/components/world-clock/index.js
+++ labs/src/components/world-clock/index.js
@@ -1,10 +1,29 @@
 import './style.css'
 
-function WorldClock() {
+function WorldClock({tenThanhPho, muiGio}) {
+
+    function convertTime(offset) {
+        // create Date object for current location
+        var localTime = new Date();
+        
+        // convert to msec
+        // subtract local time zone offset
+        // get UTC time in msec
+        const utcTime = localTime.getTime() + (localTime.getTimezoneOffset() * 60000);
+        
+        // create new Date object for different city
+        // using supplied offset
+        
+        const cityTime = new Date(utcTime + (3600000*offset));
+        
+        // return time as a string
+        return cityTime.toLocaleString()
+        }
+
     return (
         <div className='world-clock'>
-            <div className="city-name">HaNoi</div>
-            <div className="city-clock">21:00</div>
+            <div className="city-name">{tenThanhPho}</div>
+            <div className="city-clock">{convertTime(muiGio)}</div>
         </div>
     )
 }
