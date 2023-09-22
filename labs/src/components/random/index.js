7fc262960298393e91c601f8bc3a269d9b491be7
Mac <mac@MacBook-Pro-cua-Mac.local>
2023-09-22 09:11:24 +0700

    lab2

--- labs/src/components/random/index.js
+++ labs/src/components/random/index.js
@@ -5,26 +5,42 @@ import './style.css';
 
 
 function RandomNumber() {
+    const [min, setMin] = useState (0);
+    const [max, setMax] = useState (0);
+    const [random,setRandom] = useState (0);
+
+    function handleMinChange (event) {
+        setMin(event.target.value)
+        // const value = event.target.value
+        // setMin (Number(value))
+    }
+
+    const handleMaxChange = (event) => {
+        // console.log(event)
+        const value = event.target.value
+        setMax(Number(value))
+    }
+
+    function handleGenerateButtonClick() {
+        const value = Math.floor(Math.random() * (max - min)) + min;
+        setRandom(value)
+    }
+
+    
+
     return (
         <div className="wrapper-random-number">
             <div className="wrapper-first-row">
                 <div className="random-result">
-                    2
+                    {random}
                 </div>
                 <div className="wrapper-min-max">
-                    <TextField 
-                        className='wrapper-min' 
-                        id="standard-basic" 
-                        label="min" 
-                        variant="standard" />
-                    <TextField 
-                        className='wrapper-max' 
-                        id="standard-basic" 
-                        label="max" 
-                        variant="standard" />
+                    <TextField className='wrapper-min' id="standard-basic" label="min" variant="standard" onChange={handleMinChange}/>
+                    <TextField className='wrapper-max' id="standard-basic" label="max" variant="standard" onChange={handleMaxChange}/>
+                        
                 </div>
             </div>
-            <Button className='random-button' variant="contained">generate</Button>
+            <Button className='random-button' variant="contained" onClick={handleGenerateButtonClick}>generate</Button>
         </div>
     )
 }
